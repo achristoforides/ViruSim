d52c8f2eef72121cd2ffdc2b23ea7744cbec8ec6
cdico <cdicovskiy97@gmail.com>
2020-04-14 17:28:11 -0400

    movement is good

--- sim.py
+++ sim.py
@@ -5,6 +5,7 @@ import pygame
 import random as rd
 import math
 import pygame.math
+import pygame.gfxdraw
 
 from pygame.locals import *
 
@@ -12,11 +13,14 @@ vec = pygame.math.Vector2
 
 rd.seed(69)
 
+WIDTH = 885
+HEIGHT = 600
+
 SUSCEPTIBLE = 0
 INFECTED = 1
 RECOVERED = 2
 
-MAX_SPEED = 3
+MAX_SPEED = 0.25
 
 DIRECTION_CHANGE_CHANCE = 0.05
 
@@ -30,25 +34,37 @@ def color(state):
 
 class Agent(pygame.sprite.Sprite):
 
-    def __init__(self, id, state, ctrl):
+    def __init__(self, id, state):
         pygame.sprite.Sprite.__init__(self)
         self.id = id
         self.state = state
         self.color = color(state)
-        self.pos = vec(rd.randint(1, ctrl.x_size - 1), rd.randint(1, ctrl.y_size - 1))
+        self.x = rd.randint(1, WIDTH - 1)
+        self.y = rd.randint(1, HEIGHT - 1)
+        self.pos = vec(self.x, self.y)
         #self.speed = 2
-        self.size = 10
-        self.image = pygame.Surface((self.size, self.size))
-        pygame.draw.circle(screen, self.color, self.pos, self.size)
+        self.size = 15
+        self.image = pygame.Surface((self.size, self.size), pygame.SRCALPHA)
+        
+        #pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+
+        pygame.gfxdraw.aacircle(self.image, 0, 0, 50, self.color)
+        pygame.gfxdraw.filled_circle(self.image, 0, 0, 50, self.color)
+
+        self.original_image = self.image
         self.rect = self.image.get_rect(center=self.pos)
+
         self.vel = vec(0, 0)
         self.accel = vec(0, -0.2)
         self.angle_speed = 0
         self.angle = 0
 
-    def update(self):
-        if self.state = INFECTED:
+    def update(self, all_agents):
+        if self.state == INFECTED:
             self.recover_check()
+
+        #if self.state == SUSCEPTIBLE:
+            self.infect_check(all_agents)
 
         if rd.random() > DIRECTION_CHANGE_CHANCE:
             self.change_dir()
@@ -59,7 +75,10 @@ class Agent(pygame.sprite.Sprite):
             self.vel.scale_to_length(MAX_SPEED)
 
         self.pos += self.vel
-        self.rect.center = self.position
+        self.rect.center = self.pos
+
+        self.wrap_around_screen()
+
 
     def change_dir(self):
         if rd.random() > 0.5:
@@ -84,20 +103,34 @@ class Agent(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect(center=self.rect.center)
         
-        
-    def move(self, ctrl):
-        self.dir = rd.randint(1, 4)
 
-        if self.dir == 1:
-            self.pos[0] += self.speed
-        elif self.dir == 2:
-            self.pos[0] -= self.speed
-        elif self.dir == 3:
-            self.pos[1] += self.speed
-        else:
-            self.pos[1] -= self.speed
+    def wrap_around_screen(self):
         
-    def infect(self):
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.y <= 0:
+            self.pos.y = HEIGHT
+        if self.pos.y > HEIGHT:
+            self.pos.y = 0
+
+            
+    def infect_check(self, all_agents):
+        
+        agents = pygame.sprite.spritecollide(self, all_agents, False)
+
+        for agent in agents:
+            #print("AA")
+
+            agent = Agent(agent.id, INFECTED)
+            
+            #self.status = INFECTED
+            #self.color = color(agent.state)
+
+            #pygame.gfxdraw.aacircle(agent.image, 0, 0, 50, agent.color)
+            #pygame.gfxdraw.filled_circle(agent.image, 0, 0, 50, agent.color)
+        
         return
         
     def recover_check(self):
@@ -130,7 +163,7 @@ class Controller():
         
 pygame.init()
 
-ctrl = Controller(885, 600, 250, 10)
+ctrl = Controller(WIDTH, HEIGHT, 100, 2)
 
 screen = pygame.display.set_mode([ctrl.x_size, ctrl.y_size])
 clock = pygame.time.Clock()
@@ -139,22 +172,24 @@ all_agents = pygame.sprite.Group()
 
 for i in range(0, ctrl.agent_count):
     if i < ctrl.starting_infected:
-        agents.append(Agent(i, INFECTED, ctrl))
+        all_agents.add(Agent(i, INFECTED))
     else:
-        agents.append(Agent(i, SUSCEPTIBLE, ctrl))
+        all_agents.add(Agent(i, SUSCEPTIBLE))
 
+        
 while ctrl.running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ctrl.running = False
 
+    all_agents.update(all_agents)
 
     screen.fill((0, 0, 0))
 
-    all_agents.update()
+    all_agents.draw(screen)
 
-    pygame.display.flip()
+    pygame.display.update()
 
 pygame.quit()
         
